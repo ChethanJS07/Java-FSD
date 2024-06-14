fb7a65c7c53ca5cdd7c687ae3f17adf494600a9e
ChethanJS07 <jschethan75@gmail.com>
2024-06-14 15:26:42 +0530

    Session 6 - JavaScript Introduction

---
+++ script1.js
@@ -0,0 +1,18 @@
+console.log("Hello world");
+//Single line comment
+/* Multi line 
+comment */
+console.log("Comments are not executed");
+
+//Variable can be defined in 3 ways : let, const, *var(old way of declaring);
+let myvar = 32;
+let x = 45;
+y = "hello";
+console.log(x+myvar);
+p = true;
+q = false;
+arr = [1,2,3];
+myObj = {'key1':1,'key2':2,'key3':3};
+console.log(q,p);
+console.log(4-'3');
+console.log(Boolean({}));
